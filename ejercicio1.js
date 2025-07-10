d4618716958d2188d32b95e6be18c4d6af54660a
yohana993 <nyvillarraga@gmail.com>
2025-07-10 17:06:10 -0500

    agregar

---
+++ ejercicio1.js
@@ -0,0 +1,12 @@
+
+let nombre = "Yohana"
+
+console.log(nombre);
+
+let edad = 30;
+
+console.log(edad);
+
+let esEstudiante = true;
+
+console.log(esEstudiante);
---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+let dato = undefined;
+
+console.log(dato);
+
+let tomate = null;
+
+console.log(tomate);
+
+
+
